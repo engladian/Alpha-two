afed784f1e64457798d95abc581b490bfdd1a0fa
engladian <simon.franklin2@cigna.com>
2015-09-21 09:47:37 +1200

    First cut
    First cut

---
+++ app/heron-app-views.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+var controllersModule = angular.module('heronAppViews.Controllers', ['ngRoute']);
+
+//Shared Routing
+controllersModule.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+         .when('/home', {
+             templateUrl: 'home/start-page.html'
+         })
+         .when('/countries', {
+             templateUrl: 'countries/countries.html',
+             controller: 'CountriesController',
+             resolve: {
+                 countries: ['countriesFactory', function (countriesFactory) {
+                     return countriesFactory.getCountries();
+                 }]
+             }
+         })
+        .when('/countries/:country/:capital', {
+            templateUrl: 'countrydetails/countrydetails.html',
+            controller: 'CountryDetailsController',
+            resolve: {
+                capitalPop: ['countriesFactory', '$route', function (countriesFactory, $route) {
+                    var capital = $route.current.params.capital;
+                    var countryCode = $route.current.params.country;
+                    return countriesFactory.getCapitalPopulation(countryCode, capital);
+                }],
+                neighbours: ['countriesFactory', '$route', function (countriesFactory, $route) {
+                    var countryCode = $route.current.params.country;
+                    return countriesFactory.getNeighbours(countryCode);
+                }]
+            }
+        })
+        .otherwise('/home');
+}]);
---
+++ app/heron-app.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+var appModule = angular.module('heronApp',['heronAppViews',function() {
+}]);
+
+appModule.controller('viewController', ['ngRoute', function() {
+    
+
+
+
+}]);
+
+appModule.directive('firstInsured', function() {
+    return {
+    restrict: 'E',
+        templateUrl: './components/shared-elements/firstinsured.html'
+    }
+});
+
+appModule.directive('secondInsured', function () {
+    return {
+        restrict: 'E',
+        templateUrl: './components/shared-elements/secondinsured.html'
+    }
+});
